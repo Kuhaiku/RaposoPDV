af694674ca9b86a0c78e320e5aba1a6f84a0073d
Raposobl <leonardoraposoboechat@gmail.com>
2025-09-22 20:57:55 -0300

    multer att

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,13 +1,11 @@
 const multer = require('multer');
 
-// Configuração base de armazenamento em memória
 const storage = multer.memoryStorage();
 
-// Middleware para upload de IMAGENS
-const uploadImage = multer({
+const uploadImages = multer({
     storage: storage,
     limits: {
-        fileSize: 7 * 1024 * 1024 // Limite de 7MB
+        fileSize: 7 * 1024 * 1024
     },
     fileFilter: (req, file, cb) => {
         const mimeTypes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif', 'image/webp'];
@@ -17,13 +15,12 @@ const uploadImage = multer({
             cb(new Error('Tipo de arquivo inválido. Apenas imagens (jpg, png, gif, webp) são permitidas.'), false);
         }
     }
-}).single('imagem'); // Nome do campo para imagem
+}).array('imagens', 5);
 
-// Middleware para upload de CSV
 const uploadCsv = multer({
     storage: storage,
     limits: {
-        fileSize: 7 * 1024 * 1024 // Limite de 7MB
+        fileSize: 7 * 1024 * 1024
     },
     fileFilter: (req, file, cb) => {
         if (file.mimetype === 'text/csv') {
@@ -32,6 +29,6 @@ const uploadCsv = multer({
             cb(new Error('Tipo de arquivo inválido. Apenas arquivos .csv são permitidos.'), false);
         }
     }
-}).single('csvfile'); // Nome do campo para CSV
+}).single('csvfile');
 
-module.exports = { uploadImage, uploadCsv };
+module.exports = { uploadImages, uploadCsv };
